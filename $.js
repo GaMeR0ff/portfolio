5f88877f0afc852f77e3ec4cb6b3061c7318c92a
GaMeR0ff <PodarkiPort.noreply.github.com>
2018-12-16 11:02:39 +0400

    Initial commit

---
+++ $.js
@@ -0,0 +1,293 @@
+
+
+
+$("#r").click(function(){
+$("#skla").css("display" , "flex")
+});
+$("#skla").click(function(e){
+    if(this === e.target){$("#skla").css("display" , "none")}
+});
+$('#tr').click(function(){
+    kl1=document.getElementById("kl1");
+if( kl1.src = 'img/klava.png'  || 'img/klava3.png'){kl1.src = "img/Klava2.png";}
+});
+$('#tr1').click(function(){
+    kl1=document.getElementById("kl1");
+if( kl1.src = 'img/klava.png'  || "img/Klava2.png"){kl1.src = "img/klava3.png";}
+});
+$('#tr2').click(function(){
+    kl1=document.getElementById("kl1");
+if( kl1.src = 'img/Klava2.png'  || 'img/klava3.png'){kl1.src = "img/klava.png";}
+});
+
+
+
+
+$("#r1").click(function(e){
+    $("#ssou").css("display" , "flex")
+    });
+$("#ssou").click(function(e){
+    if(this === e.target){$("#ssou").css("display" , "none")}
+});
+$('#so1').click(function(){
+    so1=document.getElementById("so");
+if( so1.src = 'img/sound.png'  || 'img/sound3.png'){so1.src = "img/sound2.png";}
+});
+$('#so2').click(function(){
+    so1=document.getElementById("so");
+if( so1.src = 'img/sound.png'  || "img/sound2.png"){so1.src = "img/sound3.png";}
+});
+$('#so3').click(function(){
+    so1=document.getElementById("so");
+if( so1.src = 'img/sound2.png'  || 'img/sound3.png'){so1.src = "img/sound.png";}
+});
+
+
+
+$("#r2").click(function(){
+    $("#sps").css("display" , "flex")
+    });
+$("#sps").click(function(e){
+    if(this === e.target){$("#sps").css("display" , "none")}
+});
+$('#ps1').click(function(){
+    ps1=document.getElementById("ps");
+if( ps1.src = 'img/ps_1.png'  || 'img/ps3.png'){ps1.src = "img/ps2.png";}
+});
+$('#ps2').click(function(){
+    ps1=document.getElementById("ps");
+if( ps1.src = 'img/ps_1.png'  || "img/ps2.png"){ps1.src = "img/ps3.png";}
+});
+$('#ps3').click(function(){
+    ps1=document.getElementById("ps");
+if( ps1.src = 'img/ps2.png'  || 'img/ps3.png'){ps1.src = "img/ps_1.png";}
+});
+
+
+$("#r3").click(function(){
+    $("#skres").css("display" , "flex")
+    });
+$("#skres").click(function(e){
+    if(this === e.target){$("#skres").css("display" , "none")}
+});
+$('#kres1').click(function(){
+    ps1=document.getElementById("kres");
+if( ps1.src = 'img/kres1.png'  || 'img/kres3.png'){ps1.src = "img/kres2.png";}
+});
+$('#kres2').click(function(){
+    ps1=document.getElementById("kres");
+if( ps1.src = 'img/kres1.png'  || "img/kres2.png"){ps1.src = "img/kres3.png";}
+});
+$('#kres3').click(function(){
+    ps1=document.getElementById("kres");
+if( ps1.src = 'img/kres2.png'  || 'img/kres3.png'){ps1.src = "img/kres1.png";}
+});
+
+
+
+$("#r4").click(function(){
+    $("#svr").css("display" , "flex")
+    });
+$("#svr").click(function(e){
+    if(this === e.target){$("#svr").css("display" , "none")}
+});
+$('#vr1').click(function(){
+    ps1=document.getElementById("vr");
+if( ps1.src = 'img/vr1.png'  || 'img/vr3.png'){ps1.src = "img/vr2.png";}
+});
+$('#vr2').click(function(){
+    ps1=document.getElementById("vr");
+if( ps1.src = 'img/vr1.png'  || "img/vr2.png"){ps1.src = "img/vr3.png";}
+});
+$('#vr3').click(function(){
+    ps1=document.getElementById("vr");
+if( ps1.src = 'img/vr2.png'  || 'img/vr3.png'){ps1.src = "img/vr1.png";}
+});
+
+
+$("#r5").click(function(){
+    $("#sKL").css("display" , "flex")
+    });
+$("#sKL").click(function(e){
+    if(this === e.target){$("#sKL").css("display" , "none")}
+});
+$('#KL1').click(function(){
+    ps1=document.getElementById("KLAVA");
+if( ps1.src = 'img/KLAVA_1.png'  || 'img/KLAVA_3.png'){ps1.src = "img/KLAVA_2.png";}
+});
+$('#KL2').click(function(){
+    ps1=document.getElementById("KLAVA");
+if( ps1.src = 'img/KLAVA_1.png'  || "img/KLAVA_2.png"){ps1.src = "img/KLAVA_3.png";}
+});
+$('#KL3').click(function(){
+    ps1=document.getElementById("KLAVA");
+if( ps1.src = 'img/KLAVA_2.png'  || 'img/KLAVA_3.png'){ps1.src = "img/KLAVA_1.png";}
+});
+
+
+$("#r6").click(function(){
+    $("#smouse").css("display" , "flex")
+    });
+$("#smouse").click(function(e){
+    if(this === e.target){$("#smouse").css("display" , "none")}
+});
+$('#mouse1').click(function(){
+    ps1=document.getElementById("mouse");
+if( ps1.src = 'img/mouse1.png'  || 'img/mouse3.png'){ps1.src = "img/mouse2.png";}
+});
+$('#mouse2').click(function(){
+    ps1=document.getElementById("mouse");
+if( ps1.src = 'img/mouse1.png'  || "img/mouse2.png"){ps1.src = "img/mouse3.png";}
+});
+$('#mouse3').click(function(){
+    ps1=document.getElementById("mouse");
+if( ps1.src = 'img/mouse2.png'  || 'img/mouse3.png'){ps1.src = "img/mouse1.png";}
+});
+
+$("#r7").click(function(){
+    $("#splanner").css("display" , "flex")
+    });
+$("#splanner").click(function(e){
+    if(this === e.target){$("#splanner").css("display" , "none")}
+});
+$('#planner1').click(function(){
+    ps1=document.getElementById("planner");
+if( ps1.src = 'img/planner1.png'  || 'img/planner3.png'){ps1.src = "img/planner2.png";}
+});
+$('#planner2').click(function(){
+    ps1=document.getElementById("planner");
+if( ps1.src = 'img/planner1.png'  || "img/planner2.png"){ps1.src = "img/planner3.png";}
+});
+$('#planner3').click(function(){
+    ps1=document.getElementById("planner");
+if( ps1.src = 'img/planner2.png'  || 'img/planner3.png'){ps1.src = "img/planner1.png";}
+});
+
+
+$("#r8").click(function(){
+    $("#slodka").css("display" , "flex")
+    });
+$("#slodka").click(function(e){
+    if(this === e.target){$("#slodka").css("display" , "none")}
+});
+$('#lodka1').click(function(){
+    ps1=document.getElementById("lodka");
+if( ps1.src = 'img/lodka1.png'  || 'img/lodka3.png'){ps1.src = "img/lodka2.png";}
+});
+$('#lodka2').click(function(){
+    ps1=document.getElementById("lodka");
+if( ps1.src = 'img/lodka1.png'  || "img/lodka2.png"){ps1.src = "img/lodka3.png";}
+});
+$('#lodka3').click(function(){
+    ps1=document.getElementById("lodka");
+if( ps1.src = 'img/lodka2.png'  || 'img/lodka3.png'){ps1.src = "img/lodka1.png";}
+});
+
+$("#r9").click(function(){
+    $("#secsc").css("display" , "flex")
+    });
+$("#secsc").click(function(e){
+    if(this === e.target){$("#secsc").css("display" , "none")}
+});
+$('#ecsc1').click(function(){
+    ps1=document.getElementById("ecsc");
+if( ps1.src = 'img/ecsc1.png'  || 'img/ecsc3.png'){ps1.src = "img/ecsc2.png";}
+});
+$('#ecsc2').click(function(){
+    ps1=document.getElementById("ecsc");
+if( ps1.src = 'img/ecsc1.png'  || "img/ecsc2.png"){ps1.src = "img/ecsc3.png";}
+});
+$('#ecsc3').click(function(){
+    ps1=document.getElementById("ecsc");
+if( ps1.src = 'img/ecsc2.png'  || 'img/ecsc3.png'){ps1.src = "img/ecsc1.png";}
+});
+
+
+$("#r10").click(function(){
+    $("#sbagi").css("display" , "flex")
+    });
+$("#sbagi").click(function(e){
+    if(this === e.target){$("#sbagi").css("display" , "none")}
+});
+$('#bagi1').click(function(){
+    ps1=document.getElementById("bagi");
+if( ps1.src = 'img/bagi1.png'  || 'img/bagi3.png'){ps1.src = "img/bagi2.png";}
+});
+$('#bagi2').click(function(){
+    ps1=document.getElementById("bagi");
+if( ps1.src = 'img/bagi1.png'  || "img/bagi2.png"){ps1.src = "img/bagi3.png";}
+});
+$('#bagi3').click(function(){
+    ps1=document.getElementById("bagi");
+if( ps1.src = 'img/bagi2.png'  || 'img/bagi3.png'){ps1.src = "img/bagi1.png";}
+});
+
+
+$("#r11").click(function(){
+    $("#srdflh").css("display" , "flex")
+    });
+$("#srdflh").click(function(e){
+    if(this === e.target){$("#srdflh").css("display" , "none")}
+});
+$('#rdflh1').click(function(){
+    ps1=document.getElementById("rdflh");
+if( ps1.src = 'img/rdflh1.png'  || 'img/rdflh3.png'){ps1.src = "img/rdflh2.png";}
+});
+$('#rdflh2').click(function(){
+    ps1=document.getElementById("rdflh");
+if( ps1.src = 'img/rdflh1.png'  || "img/rdflh2.png"){ps1.src = "img/rdflh3.png";}
+});
+$('#rdflh3').click(function(){
+    ps1=document.getElementById("rdflh");
+if( ps1.src = 'img/rdflh2.png'  || 'img/rdflh3.png'){ps1.src = "img/rdflh1.png";}
+});
+
+
+$("#r12").click(function(){
+    $("#skvadr").css("display" , "flex")
+    });
+$("#skvadr").click(function(e){
+    if(this === e.target){$("#skvadr").css("display" , "none")}
+});
+$('#kvadr1').click(function(){
+    ps1=document.getElementById("kvadr");
+if( ps1.src = 'img/kvadr1.png'  || 'img/kvadr3.png'){ps1.src = "img/kvadr2.png";}
+});
+$('#kvadr2').click(function(){
+    ps1=document.getElementById("kvadr");
+if( ps1.src = 'img/kvadr1.png'  || "img/kvadr2.png"){ps1.src = "img/kvadr3.png";}
+});
+$('#kvadr3').click(function(){
+    ps1=document.getElementById("kvadr");
+if( ps1.src = 'img/kvadr2.png'  || 'img/kvadr3.png'){ps1.src = "img/kvadr1.png";}
+});
+
+
+
+$("#SOS").click(function(){
+   $("#sos").css("display" , "flex");
+   $("#OBRAZ").css("display" , "flex");
+   $("#OBRAZBUT").css("display" , "flex")
+   });
+
+   $("#OBRAZBUT").click(function(){
+    $("#sobraz").css("display" , "flex");
+    $("#sos").css("display" , "none");
+    $("#OBRAZ").css("display" , "none");
+    $("#OBRAZBUT").css("display" , "none")
+    });
+$("#sobraz").click(function(e){
+    if(this === e.target){$("#sobraz").css("display" , "none")}
+});
+$('#fhoto1').click(function(){
+    ps1=document.getElementById("obraz");
+if( ps1.src = 'img/fhoto2.png'  || 'img/fhoto3.png'){ps1.src = "img/fhoto1.png";}
+});
+$('#fhoto2').click(function(){
+    ps1=document.getElementById("obraz");
+if( ps1.src = 'img/fhoto1.png'  || "img/fhoto3.png"){ps1.src = "img/fhoto2.png";}
+});
+$('#fhoto3').click(function(){
+    ps1=document.getElementById("obraz");
+if( ps1.src = 'img/fhoto2.png'  || 'imgfhotor1.png'){ps1.src = "img/fhoto3.png";}
+});
